c9d3c717c6cb9a633dc3fe009d0c5222c8f997cf
FfrftTK <fr5tdkr20@gmail.com>
2026-03-31 15:28:00 +0900

    improve theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,22 +1,66 @@
 local wezterm = require("wezterm")
-local config = {}
+local config = wezterm.config_builder()
 
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+config.automatically_reload_config = true
 
--- colors
-config.color_scheme = "AdventureTime"
-config.window_background_opacity = 0.5
+-- theme
+config.window_background_opacity = 0.65
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
 
--- font
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
+-- typography
 config.font_size = 13.0
+
+-- input
+config.use_ime = true
 
 -- keybinds
 -- config.disable_default_key_bindings = true
 -- local keybind = require 'keybinds'
 config.keys = {
-	{ key = "D", mods = "CMD", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "[", mods = "CTRL", action = wezterm.action.PaneSelect },
+	{ key = "H", mods = "CMD", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "V", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "C", mods = "SHIFT|CMD", action = wezterm.action.ActivateCopyMode },
 }
 -- config.key_tables = keybind.key_tables
